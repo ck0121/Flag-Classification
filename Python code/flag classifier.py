0c4840063f69cdbedbc3f8ce7d2e740ea1ff382b
ck0121 <70354769+ck0121@users.noreply.github.com>
2020-09-03 11:33:52 -0400

    code update

--- Python code/flag classifier.py
+++ Python code/flag classifier.py
@@ -60,7 +60,68 @@ def forward(self):
     x = self.fc3(x)
     return x
     
+for epoch in range(epochs):
+    for batch in train_loader:
+        optimizer.zero_grad()
+        input, target = batch
+        output = model(input)
+        loss = loss_fn(output, target)
+        loss.backward()
+        optimizer.step()
+        
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+else:
+    device = torch.device("cpu")
     
+model.to(device)
+
+
+def train(model, optimizer, loss_fn, train_loader, val_loader,
+          epochs=20, device="cpu"):
+    for epoch in range(epochs):
+        training_loss = 0.0
+        valid_loss = 0.0
+        model.train()
+        for batch in train_loader:
+            optimizer.zero_grad()
+            inputs, target = batch
+            inputs = inputs.to(device)
+            target = targets.to(device)
+            output = model(inputs)
+            loss = loss_fn(output, target)
+            loss.backward()
+            optimizer.step()
+            training_loss += loss.data.item()
+        training_loss /= len(train_iterator)
+        
+        model.eval()
+        num_correct = 0
+        num_example = 0
+        for batch in val_loader:
+            inputs, targets = batch
+            inputs = inputs.to(device)
+            output = model(inputs)
+            targets = targets.to(device)
+            loss = loss_fn(output,targets)
+            valid_loss += loss.data.item()
+            correct = torch.eq(torch.max(F.softmax(output), dim=1)[1],
+                                                            target).view(-1)
+            num_correct += torch.sum(correct).item()
+            num_examples += correct.shape[0]
+        valid_loss /= len(valid_iterator)
+        
+        print('Epoch: {}, Training Loss: {:.2f}, Validation Loss:{:.2f},accuracy = {:.2f}'.
+              format(epoch, training_loss, valid_loss, num_correct / num_examples))
+        
+        
+train(simplenet, optimizer, torch.nn.CrossEntropyLoss(),
+      train_data_loader, test_data_loader,device)
+
+
+
+        
+        
     
     
     
