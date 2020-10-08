e8a401b5a3762b038efb470938f895349da4cd13
ck0121 <70354769+ck0121@users.noreply.github.com>
2020-10-08 14:15:33 -0400

    detectron2

--- Detectron2/xml_to_csv.py
+++ Detectron2/xml_to_csv.py
@@ -1,18 +1,11 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Oct  6 13:41:56 2020
-
-@author: ck
-"""
-
 import os
 import glob
 import pandas as pd
 import xml.etree.ElementTree as ET
 
 
-
 def xml_to_csv(path):
+    xml_list = []
     for xml_file in glob.glob(path + '/*.xml'):
         tree = ET.parse(xml_file)
         root = tree.getroot()
@@ -27,14 +20,17 @@ def xml_to_csv(path):
                      int(member[4][3].text)
                      )
             xml_list.append(value)
-        column_name = ['filename', 'width', 'height', 'calss', 'xmin', 'ymin', 'xmax', 'ymax']
-        xml_df = pd.DataFrame(xml_list, columns=column_name)
-        return xml_df
+    column_name = ['filename', 'width', 'height', 'class', 'xmin', 'ymin', 'xmax', 'ymax']
+    xml_df = pd.DataFrame(xml_list, columns=column_name)
+    return xml_df
 
 
 def main():
     for folder in ['train', 'test']:
-        image_path = os.path.join(os.getcwd(), ('images/' + folder))
+        image_path = os.path.join(os.getcwd(), (folder))
         xml_df = xml_to_csv(image_path)
-        xml_df.to_csv(('images/'+folder+'_labels.csv'), index=None)
+        xml_df.to_csv((folder+'_labels.csv'), index=None)
     print('Successfully converted xml to csv.')
+
+
+main()
